1c221968e247a8477799a5c7347f2dc8e7fc96db
Samodh Fernando <108604168+samodhfdo@users.noreply.github.com>
2024-06-28 00:55:06 -0700

    Add files via upload

---
+++ Basic Arithmetics.py
@@ -0,0 +1,10 @@
+x = input("Enter your first number : ")
+y = input("Enter your second number : ")
+
+num1 = int(x)
+num2 = int(y)
+
+print (f"Addition : {num1+num2}")
+print (f"Substraction : {num1-num2}")
+print (f"Multiplication : {num1*num2}")
+print (f"Division : {num1/num2}")
